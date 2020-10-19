fe67bf4af31c913692879c1e5cb998e18b9fd4b5
Nikolay Filiuk <Filiuk98@gmail.com>
2020-10-19 13:43:23 +0300

    Updates

---
+++ precache-manifest.fe8b647472319d69b6d3d1cd397ff727.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e2118e165b36299d1b52c02b2fe73e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "40e28d2f58b0c01b7913",
+    "url": "./static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "40e28d2f58b0c01b7913",
+    "url": "./static/js/2.c88a4d3a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c88a4d3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "068fc5e57c2af69fd060",
+    "url": "./static/js/main.42d97c8e.chunk.js"
+  },
+  {
+    "revision": "c2acbe53827daed63a5f",
+    "url": "./static/js/runtime-main.1aec85e4.js"
+  }
+]);
